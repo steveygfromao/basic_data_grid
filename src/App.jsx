f1c77684bf9e5bfd141fadfb88378a7bad4d37a4
Steve Green <121233991+steveygfromao@users.noreply.github.com>
2024-05-20 08:42:15 +0100

    added sortable grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import BasicGrid from './components/BasicGrid'
+import InitialSort from './components/InitialSort'
 
 function App() {
 
   return (
     <>
       <div>
+        <h2>Basic Grid</h2>
         <BasicGrid />
+        <h2>Sortable Grid</h2>
+        <InitialSort />
       </div>
     </>
   )
